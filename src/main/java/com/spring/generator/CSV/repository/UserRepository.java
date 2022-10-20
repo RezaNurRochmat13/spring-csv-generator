7c3a1bb4b54dd8655399fe8636b88efd74615a90
Reja <rezanurrochmat@gmail.com>
2022-10-20 13:56:25 +0700

    add repository

---
+++ src/main/java/com/spring/generator/CSV/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.spring.generator.CSV.repository;
+
+import com.spring.generator.CSV.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
